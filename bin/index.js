87ad1ce57b02a4e222a25a67a2c805dcbe0295d0
Rico <dcy10000@gmail.com>
2018-02-24 22:07:51 +0800

    add test delay

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ const program = require('commander')
 const pkgInfo = require('../package.json')
 const Conf = require('conf')
 const config = new Conf()
-const localConfig = new Conf({configName: 'local'})
+const localConfig = new Conf({ configName: 'local' })
 
 if (!config.has('servers')) config.set('servers', []) // Fix servers undefined problem
 if (!config.has('default')) config.set('default', []) // Default set
@@ -18,12 +18,13 @@ if (localConfig.size === 0) localConfig.store = {
 program
   .version('v' + pkgInfo.version)
   .usage('[options] {command} ...')
-  .command('welcome', 'show Welcome Message', {isDefault: true})
+  .command('welcome', 'show Welcome Message', { isDefault: true })
   .command('config [path]', 'set ssr python client\'s location')
   .command('add', 'add a host mannually')
   .command('add [uri]', 'add a SSR URI')
   .command('connect', 'set the default host and connect it')
   .command('ls', 'list all hosts by group and you can view their config')
+  .command('delay', 'test routes delay')
   .command('edit', 'list all hosts by group and you can edit their config')
   .command('local', 'edit SSR\'s local config, need reconnect after config')
   .command('rm', 'remove a host by list')
