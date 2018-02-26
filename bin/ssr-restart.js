d12a78fa67feaaef01bd1ec362424be030b7292a
Rico <dcy10000@gmail.com>
2018-02-26 19:36:57 +0800

    appoint file

--- bin/ssr-restart.js
+++ bin/ssr-restart.js
@@ -1,11 +1,15 @@
 const Program = require('commander')
 const Exec = require('../lib/exec')
 const Dynamic = require('../lib/dynamic')
-Program.option("-d, --dynamic", "Dynamic config file.for docker or other use")
+Program.option("-d, --dynamic [filename]", "Dynamic config file.for docker or other use")
     .parse(process.argv)
 
 if (Program.dynamic) {
-    let config_file = Dynamic.generateConfigFile();
+    if (Program.dynamic === true) {
+        console.warn("please add filename option")
+        return;
+    }
+    let config_file = Dynamic.generateConfigFile(Program.dynamic);
     Exec('restart', false, config_file.path)
 }
 else {
--- bin/ssr-start.js
+++ bin/ssr-start.js
@@ -1,11 +1,16 @@
+
+const Dynamic = require('../lib/dynamic');
 const Program = require('commander')
 const Exec = require('../lib/exec')
-const Dynamic = require('../lib/dynamic')
-Program.option("-d, --dynamic", "Dynamic config file.for docker or other use")
+Program.option("-d, --dynamic [filename]", "Dynamic config file.for docker or other use")
     .parse(process.argv)
 
 if (Program.dynamic) {
-    let config_file = Dynamic.generateConfigFile();
+    if (Program.dynamic === true) {
+        console.warn("please add filename option")
+        return;
+    }
+    let config_file = Dynamic.generateConfigFile(Program.dynamic);
     Exec('start', false, config_file.path)
 }
 else {
